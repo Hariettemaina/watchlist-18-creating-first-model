799f587e35e7650bbfa414be17ddf2f1092bc29b
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-07 00:48:03 +0300

    create user database

--- config.py
+++ config.py
@@ -4,14 +4,18 @@ class Config:
 
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=5e54ded54dd3a867c2d020ff50a5d40d'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carolmaina:ethan2017@localhost/watchlist'
+
     #SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:29654387@localhost/watchlist'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+   
 
     @staticmethod
     def init_app(app):
         pass
 
+
+# SECRET_KEY = os.urandom(32)
+#app.config['SECRET_KEY'] = SECRET_KEY
 
 class ProdConfig(Config):
     pass
